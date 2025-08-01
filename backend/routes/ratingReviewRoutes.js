36b31dbf080d82b5f3f43aa624ad4207449908a9
Bimash Thapa <mailbimashthapa@gmail.com>
2025-08-01 20:34:41 +0545

    Update ratingReviewRoutes.js

--- backend/routes/ratingReviewRoutes.js
+++ backend/routes/ratingReviewRoutes.js
@@ -1,24 +1,9 @@
-import { Router } from "express";
+import {Router} from "express";
 import errorHandle from "../services/errorHandler.js";
-import { isAuthenticated, restrictTo, Role } from "../middleware/authMiddleware.js";
-import { 
-    createReview, 
-    getProductReviews, 
-    getUserReviews, 
-    updateReview, 
-    deleteReview, 
-    getAllReviews 
-} from "../controllers/ratingReviewController.js";
+import { createReview } from "../controllers/ratingReviewController.js";
+import { isAuthenticated } from "../middleware/authMiddleware.js";
 
-const router = Router();
+const router=Router();
+router.route("/").post(isAuthenticated, createReview)
 
-router.route("/").post(isAuthenticated, errorHandle(createReview))
-.get(isAuthenticated, restrictTo(Role.Admin), errorHandle(getAllReviews))
-
-router.route("/myReviews").get(isAuthenticated, errorHandle(getUserReviews))
-router.route("/product/:productId").get(errorHandle(getProductReviews))
-
-router.route("/:id").patch(isAuthenticated, errorHandle(updateReview))
-.delete(isAuthenticated, errorHandle(deleteReview))
-
-export default router;
+export default router
